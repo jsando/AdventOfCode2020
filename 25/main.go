ea745148ef8fb1ae97ee66457ff348cb75aad1d6
jsando <jason.sando.lv@gmail.com>
2021-01-07 18:54:09 -0800

    its been fun

---
+++ 25/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	s := strings.Split(strings.TrimSpace(string(bytes)), "\n")
+	pk1, err := strconv.Atoi(s[0])
+	if err != nil {
+		panic(err)
+	}
+	pk2, err := strconv.Atoi(s[1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Pulic Key 1: %d, Public Key 2: %d\n", pk1, pk2)
+	loop1 := getLoopSize(7, pk1)
+	fmt.Printf("Loop size 1: %d\n", loop1)
+	loop2 := getLoopSize(7, pk2)
+	fmt.Printf("Loop size 2: %d\n", loop2)
+	fmt.Printf("Key1: %d, Key2: %d\n", createPublicKey(pk1, loop2), createPublicKey(pk2, loop1))
+}
+
+func getLoopSize(subjectNumber, publicKey int) int {
+	value := 1
+	loop := 0
+	for value != publicKey {
+		value *= subjectNumber
+		value %= 20201227
+		loop++
+	}
+	return loop
+}
+
+func createPublicKey(subjectNumber, loopSize int) int {
+	value := 1
+	for i := 0; i < loopSize; i++ {
+		value = (value * subjectNumber) % 20201227
+	}
+	return value
+}
